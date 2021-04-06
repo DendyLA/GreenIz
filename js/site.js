2cb4cc97553f293d4ee49a31efec8a4603dab98b
Dendy <74273489+DendyLA@users.noreply.github.com>
2021-04-06 06:32:01 +0300

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,110 @@
+"use strict";
+
+$(document).ready(function () {
+	/* Video Lightbox */
+	if (!!$.prototype.simpleLightboxVideo) {
+		$('.video').simpleLightboxVideo();
+	}
+
+	/*ScrollUp*/
+	if (!!$.prototype.scrollUp) {
+		$.scrollUp();
+	}
+
+	/*Responsive Navigation*/
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-trigger span").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$(this).removeClass("open");
+		} else {
+			$("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+			$(this).addClass("open");
+		}
+	});
+
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-mobile ul a").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$("#nav-trigger span").removeClass("open");
+		}
+	});
+
+	/* Sticky Navigation */
+	if (!!$.prototype.stickyNavbar) {
+		$('#header').stickyNavbar();
+	}
+
+	$('#content').waypoint(function (direction) {
+		if (direction === 'down') {
+			$('#header').addClass('nav-solid fadeInDown');
+		}
+		else {
+			$('#header').removeClass('nav-solid fadeInDown');
+		}
+	});
+
+});
+
+
+/* Preloader and animations */
+$(window).load(function () { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow-y': 'visible'});
+
+	/* WOW Elements */
+	if (typeof WOW == 'function') {
+		new WOW().init();
+	}
+
+	/* Parallax Effects */
+	if (!!$.prototype.enllax) {
+		$(window).enllax();
+	}
+
+});
+$('.team').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	centerMode: true,
+    centerPadding: '50px',
+	prevArrow:"<button type='button' class='slick-prev'><img src='../images/previous.png'> </button>",
+	nextArrow: '<button type="button" class="slick-next"><img src="../images/next.png"></button>',
+	responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true,
+			arrows: false,
+			dots: false
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+			centerPadding: '125px',
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			arrows: false,
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	  ]
+  });
+  
+	
